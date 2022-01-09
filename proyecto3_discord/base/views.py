456155eb50ea1a70f21431ca14a9cc2204397392
Amir <amir1226@gmail.com>
2022-01-09 18:18:06 -0500

    First DB migrations and adding room and message model

--- proyecto3_discord/base/views.py
+++ proyecto3_discord/base/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
+
+""" rooms = [
+    {'id': 1, 'name': 'Lets learn .net!'},
+    {'id': 2, 'name': 'Lets learn aws!'},
+    {'id': 3, 'name': 'Lets learn linux!'}
+] """
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    # room = next(item for item in rooms if item["id"] == int(pk))
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
