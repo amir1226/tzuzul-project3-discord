4d5c2ec184a7fb2931484a142dc9bae2343edcb5
Amir <amir1226@gmail.com>
2022-01-09 19:44:57 -0500

    Adding delete functionality

--- proyecto3_discord/base/views.py
+++ proyecto3_discord/base/views.py
@@ -42,3 +42,11 @@ def update_room(request, pk):
         
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj':room})
