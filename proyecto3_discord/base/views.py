73b89c1e63546d854e24c8350aa55a3167a174ac
Amir <amir1226@gmail.com>
2022-01-12 21:30:14 -0500

    Fixing update user profile form

--- proyecto3_discord/base/views.py
+++ proyecto3_discord/base/views.py
@@ -166,7 +166,7 @@ def update_user(request):
     form = UserForm(instance=user)
     
     if request.method=='POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
